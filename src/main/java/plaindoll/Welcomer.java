1d7bbc99ee144e47b1c6b7d973149a65dfc7ef81
slava1005 <slava100575@gmail.com>
2024-11-01 21:51:14 +0300

    change welcomers

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -2,7 +2,7 @@ package plaindoll;
 
 public class Welcomer{
 	public String sayWelcome() {
-		return "Welcome home, good hunter. What is it your desire?";
+                return "Welcome home, good hunter. What is it your desire?";
 	}
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
@@ -12,5 +12,8 @@ public class Welcomer{
 	}
 	public String saySome(){
 		return "something in the way";
+	}
+    public String sayHunter(){
+		return "every hunter wants to know where the pheasant sits";
 	}
 }
